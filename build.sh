568644cc30b9b624fad6fc79b383ca92c4f90740
David V. Wallin <david.wallin@ganillagrace.se>
2016-11-10 12:25:59 +0100

    upx doesnt seem to work on mac

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-APP_NAME=gow ; rm -rf $APP_NAME ; go build -ldflags="-w -s" -a -installsuffix cgo -o tmpfile ./ ; strip -x tmpfile -o $APP_NAME ; rm -rf tmpfile ; upx -9 $APP_NAME
+GOOS=darwin GOARCH=amd64 APP_NAME=gow ; rm -rf $APP_NAME ; go build -ldflags="-w -s" -a -installsuffix cgo -o tmpfile ./ ; strip -x tmpfile -o $APP_NAME ; rm -rf tmpfile
